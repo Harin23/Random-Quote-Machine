ab85b4545a1cfbc33927f9c98fa7631621cf11dc
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-03-25 01:04:41 -0600

    removed the project tester

--- script.js
+++ script.js
@@ -37,9 +37,12 @@ class App extends React.Component{
                 </div>
                 <div className="float-right text-right pr-3" id="author">-{this.state.author}</div>
                 <div className="container-fluid fixed-bottom p-3" id="icons">
-                    <a id="tweet-quote" target="_blank" className="float-left pr-2" href="twitter.com/intent/tweet"><i className="fa fa-twitter fa-3x"></i></a>
-                    <a className="float-left pr-2" href="" id="tweet-quote"><i className="fa fa-facebook fa-3x"></i></a>
-                    <a className="float-left pr-2" href="" id="tweet-quote"><i className="fa fa-instagram fa-3x"></i></a>
+                    <a id='tweet-quote' title="tweet it" target="_blank" className="button float-left pr-2" href={"https://twitter.com/intent/tweet?hashtags=MyQuoteWebsite,AutoTweetedQuote,QuoteAPI&text="
+                    +encodeURIComponent('"'+this.state.quote+'"-'+this.state.author)+"%0A"+encodeURIComponent("https://github.com/Harin23/Random-Quote-Machine")}>
+                        <i className="fa fa-twitter fa-3x"></i>
+                    </a>
+                    <a className="float-left pr-2" title="post on fb" href="" id="facebook-quote"><i className="fa fa-facebook fa-3x"></i></a>
+                    <a className="float-left pr-2" title="story on ig" href="" id="ig-quote"><i className="fa fa-instagram fa-3x"></i></a>
                     <button type="button" className="btn btn-primary float-right" id="new-quote" onClick={this.newQuote}>New Quote</button>
                 </div>
             </div>
